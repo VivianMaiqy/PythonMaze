d0444523bee094b0825216c4cbb0e3fe2666b8e9
Qiyuan Mai <qiyuan.mai@gmail.com>
2022-01-04 17:01:39 -0500

    gui.py

--- generate.py
+++ generate.py
@@ -16,19 +16,22 @@ class vertex:
             self.nei.append(self.id + size)
 
         self.select_nei = []
+        self.isSoln = False
 
 
 # dfs takes a graph G and perform dfs then return the processed graph G'
 def dfs(verList: List[vertex]):
     stack = []
+    parentMap = {}
+    size = len(verList)
+    hasSoln = False
     # Choose the initial ver, mark it as found and push it to the stack
     stack.append(verList[0])
     verList[0].found = True
     # While the stack is not empty
-    while len(stack) != 0:
+    while len(stack) != 0:        
         # Pop a ver from the stack and make it a current ver
         curr_ver = stack.pop()
-
         unfound_nei = []
         # get all un-found vertices
         for n in curr_ver.nei:
@@ -43,12 +46,20 @@ def dfs(verList: List[vertex]):
             select_ver.select_nei.append(curr_ver)
             select_ver.found = True
             stack.append(select_ver)
+            if not hasSoln:
+                parentMap[select_ver] = curr_ver
+            if select_ver.id == size - 1:
+                hasSoln = True
 
         # if current vertex has more than one unfound neighbour,
         # append current vertex to the stack
         if len(unfound_nei) > 1:
             stack.append(curr_ver)
 
+    curr = verList[size - 1]
+    while curr.id != 0:
+        verList[curr.id].isSoln = True
+        curr = parentMap[curr]
 
 # generates a maze
 def make_map(size: int) -> List[List[int]]:
